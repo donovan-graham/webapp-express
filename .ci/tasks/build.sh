f227e28c5c0df969cd1cda54d2b7bf18eea8c782
Donovan Graham <donovan@platform7.com>
2016-10-06 21:47:16 +0200

    fix: pre nodemon shrinkwrap

--- .ci/tasks/build.sh
+++ .ci/tasks/build.sh
@@ -3,7 +3,7 @@ set -ev
 
 WORK_DIR=$(pwd)
 cd $WORK_DIR/src/webapp
-npm install --no-bin-links --loglevel warn > /dev/null
+npm install --loglevel warn > /dev/null
 npm test
 npm prune --production --loglevel warn > /dev/null
 
