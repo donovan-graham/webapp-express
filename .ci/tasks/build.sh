995fcde4f243964c49af3e45c1143b2e6ca5ba1e
Donovan Graham <donovan@platform7.com>
2016-10-06 22:31:56 +0200

    fix: build

--- .ci/tasks/build.sh
+++ .ci/tasks/build.sh
@@ -7,5 +7,6 @@ npm install --no-optional --loglevel warn > /dev/null
 npm test
 npm prune --production --loglevel warn > /dev/null
 
-cp -rf $WORK_DIR/src/webapp $WORK_DIR/build
-ls -la $WORK_DIR/build
+cp -rf $WORK_DIR/src/.ci/Dockerfile $WORK_DIR/build
+mkdir -p $WORK_DIR/build/data
+cp -rf $WORK_DIR/src/webapp/* $WORK_DIR/build/data
