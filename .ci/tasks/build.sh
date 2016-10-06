b54a146c360e67417616625201cdf73ba561df9a
Donovan Graham <donovan@platform7.com>
2016-10-06 21:43:29 +0200

    fix: list dir contents

--- .ci/tasks/build.sh
+++ .ci/tasks/build.sh
@@ -6,4 +6,6 @@ cd $WORK_DIR/src/webapp
 npm install --no-bin-links --loglevel warn > /dev/null
 npm test
 npm prune --production --loglevel warn > /dev/null
-cp -rf $WORK_DIR/src/webpp $WORK_DIR/build
+
+ls -al $WORK_DIR/src/webpp
+# cp -rf $WORK_DIR/src/webpp $WORK_DIR/build
