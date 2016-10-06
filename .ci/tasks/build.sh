0fa4cf3abef4bbd511aa14cc96e3db4cee0cd9fb
Donovan Graham <donovan@platform7.com>
2016-10-06 21:30:45 +0200

    fix: trying --no-bin-link

--- .ci/tasks/build.sh
+++ .ci/tasks/build.sh
@@ -3,7 +3,7 @@ set -ev
 
 WORK_DIR=$(pwd)
 cd $WORK_DIR/src/webapp
-npm install --loglevel warn > /dev/null
+npm install --no-bin-links --loglevel warn > /dev/null
 npm test
 npm prune --production --loglevel warn > /dev/null
 cp -rf $WORK_DIR/src/webpp $WORK_DIR/build
