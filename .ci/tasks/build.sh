905583e17383002c06c6949801a1317f9bf75196
Donovan Graham <donovan@platform7.com>
2016-10-10 22:22:36 +0200

    fix: Adding build args

--- .ci/tasks/build.sh
+++ .ci/tasks/build.sh
@@ -2,11 +2,27 @@
 set -ev
 
 WORK_DIR=$(pwd)
-cd $WORK_DIR/src/webapp
+SOURCE_DIR=$WORK_DIR/src
+GIT_DIR=$SOURCE_DIR/.git
+BUILD_DIR=$WORK_DIR/build
+
+cd $SOURCE_DIR/webapp
 npm install --no-optional --loglevel warn > /dev/null
 npm test
 npm prune --production --loglevel warn > /dev/null
 
-cp -rf $WORK_DIR/src/.ci/Dockerfile $WORK_DIR/build
-mkdir -p $WORK_DIR/build/data
-cp -rf $WORK_DIR/src/webapp/* $WORK_DIR/build/data
+cp -rf $SOURCE_DIR/.ci/Dockerfile $BUILD_DIR
+mkdir -p $BUILD_DIR/data
+cp -rf $SOURCE_DIR/webapp/* $BUILD_DIR/data
+
+GIT_BRANCH=cat $GIT_DIR/HEAD | cut -d'/' -f3
+GIT_COMMIT_HASH=cat $GIT_DIR/HEAD | cut -d':' -f2 | tr -d ' ' | xargs -I FILE cat $GIT_DIR/FILE | head -c7
+GIT_URL=cat $GIT_DIR/config | grep url | cut -d'=' -f2 | tr -d ' '
+
+cat > $BUILD_DIR/build-args.json << EOL
+{
+  "GIT_URL": "$GIT_URL",
+  "GIT_BRANCH": "$GIT_BRANCH",
+  "GIT_COMMIT_HASH": "$GIT_COMMIT_HASH"
+}
+EOL
